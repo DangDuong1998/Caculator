a44e72d13bdd98c9415ad3984a997941aa4e71fa
DangDuong1998 <mvstarkfinal.ultra@gmail.com>
2025-04-10 19:07:41 +0900

    Update 06_[Bài tập] Ứng dụng tính toán đơn giản.js

--- 06_[Bài tập] Ứng dụng tính toán đơn giản.js
+++ 06_[Bài tập] Ứng dụng tính toán đơn giản.js
@@ -74,6 +74,7 @@ equal.addEventListener('click',()=>{
     }
     else if(eval(result)===2803){
         monitor.innerText = 'Chị Ly xinh!!!';
+        flag = true;
         
     else{
         monitor.innerText = eval(result);
